6850a7ad5656e105f7cc249fa3e4bbfd9d5b5c95
mannaward <alnikolaevich0@gmail.com>
2014-06-28 23:53:13 +0300

    initial commit

---
+++ src/com/example/NWatch/connection/BluetoothManager.java
@@ -0,0 +1,26 @@
+package com.example.NWatch.connection;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.os.Handler;
+import com.example.NWatch.utils.Logs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
+/**
+ * Created by AZGARD on 28.06.2014.
+ */
+public class BluetoothManager {
+
+}
